4830ccd518d67d396b8b6e42b2de22c08ce8554d
ylcdolphin <33015665+ylcdolphin@users.noreply.github.com>
2023-07-08 10:09:13 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,69 @@
+function control2 () {
+    if (E == 0 && (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1)) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, V / 2)
+    } else {
+        PID()
+    }
+}
+function PID () {
+    E = maqueen.readPatrol(maqueen.Patrol.PatrolLeft) - maqueen.readPatrol(maqueen.Patrol.PatrolRight)
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        if (preE < 0) {
+            E = -2
+        } else if (preE > 0) {
+            E = 2
+        }
+    }
+    cuE = cuE + E
+    dE = E - preE
+    preE = E
+    VD = P * E + I * cuE + D * dE
+    VL = V + VD
+    VR = V - VD
+    activate()
+}
+IR.IR_callbackUser(function (message) {
+    if (message == 1) {
+        Mode = 1
+    } else {
+        Mode = 0
+    }
+})
+function activate () {
+    if (VL < 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, VL * -1)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, VL)
+    }
+    if (VR < 0) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, VR * -1)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, VR)
+    }
+}
+let Mode = 0
+let VR = 0
+let VL = 0
+let VD = 0
+let dE = 0
+let E = 0
+let preE = 0
+let cuE = 0
+let D = 0
+let I = 0
+let P = 0
+let V = 0
+V = 100
+P = 25
+I = 0
+D = 0
+cuE = 0
+preE = 0
+basic.forever(function () {
+    if (Mode == 1) {
+        control2()
+        basic.pause(100)
+    } else {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+})
